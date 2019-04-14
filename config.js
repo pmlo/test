b4336459a61f0c23a450916f636912ebda73ec23
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-14 20:56:33 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    'ownerID': '419536423796998155',
+    'prefix': '!',
+    'fnkey': 'fb69fa20-f69f-4968-ae6b-3eb29b38a605', // Get from https://fortnitetracker.com/site-api
+    'auto_channels': ['515646335513395212'] // Channel ids for autonews and autoshop, separate with a comma
+};
+module.exports = config;
