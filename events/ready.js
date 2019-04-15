b60a1478a72c3caa17bc379d6abd2ac9767dbc92
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-15 19:02:17 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
-    client.user.setActivity("", {type: "STREAMING", url:"https://www.twitch.tv/lafrancedefortnite"});
+    client.user.setActivity(`LFDF | ${member.guild.memberCount}`, {type: "STREAMING", url:"https://www.twitch.tv/lafrancedefortnite"});
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
     client.channels.get("540107675397128202").send(`:white_check_mark: Bot **${client.user.username}** en ligne, Prêt à travailler !`);
 
