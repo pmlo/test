14832c5c28051a5aa90fd696daa96175d7c2495a
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-27 00:44:43 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,14 @@ module.exports = (client) => {
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
     client.channels.get("540107675397128202").send(`:white_check_mark: Bot **${client.user.username}** en ligne, Prêt à travailler !`);
 
-    const autoshop = () => {
-        setInterval(function() {
-            const cmd = client.commands.get('shop');
-            cmd.run(client);
-        }, 900000);
-    };
-    autoshop();
+      let interval = setInterval(() => {
+        let time = new Date();
+        let min = time.getMinutes();
+        let heur = time.getHours();
+    
+        if(min == "01" && heur == "04") {
+          var channel = client.channels.get('515646335513395212'); 
+          channel.sendMessage("!shop");
+        }
+      }, 60000);
 };
