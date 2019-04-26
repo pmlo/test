f6b15cf43bfaaa6b3d2f65bcc12446fdf1cad3ec
P M L O <48804005+pmlo@users.noreply.github.com>
2019-04-27 00:44:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
     'ownerID': '419536423796998155',
     'prefix': '!',
-    'fnkey': 'fb69fa20-f69f-4968-ae6b-3eb29b38a605', // Get from https://fortnitetracker.com/site-api
-    'auto_channels': ['515646335513395212'] // Channel ids for autonews and autoshop, separate with a comma
+    'fnkey': 'fb69fa20-f69f-4968-ae6b-3eb29b38a605'
 };
 module.exports = config;
