a8295a3fa73a3ea161d6324b4a50f8647669cce3
P M L O <48804005+pmlo@users.noreply.github.com>
2020-01-09 21:11:58 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     message.channel.send(":ping_pong: Pong ! (`"+ `${message.createdTimestamp - Date.now()}` + '` ms)');
-    client.channels.get("540107675397128202").send("Log / Utilisateur **" + message.author.username + "** / Commande **Ping**");
+    client.channels.get("664916311838687242").send("Log / Utilisateur **" + message.author.username + "** / Commande **Ping**");
 };
 
 exports.info = {
